09ac0138a2205813d53b3c5af30f626cee8ad2ba
bahrmichael <michael.bahr@live.de>
2021-02-12 22:22:57 +0100

    init

--- backend/src/functions/reactToTransaction/handler.ts
+++ backend/src/functions/reactToTransaction/handler.ts
@@ -5,11 +5,6 @@ import {DynamoDBStreamEvent, DynamoDBStreamHandler} from "aws-lambda";
 const reactToTransaction: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
   for (const record of event.Records) {
     console.log({record});
-
-    if (!['INSERT', 'MODIFY'].includes(record.eventName)) {
-      console.log(`Skipping ${record.eventName}`); // e.g. DELETE
-      continue;
-    }
   }
 }
 
